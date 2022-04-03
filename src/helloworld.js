f02598fc638b9a2cb0772e43f734e58025d17e50
周爱桩 <840043348@qq.com>
2022-04-03 17:07:43 +0800

    增加webpack-dev-server

--- src/helloworld.js
+++ src/helloworld.js
@@ -1,4 +1,4 @@
 function helloworld(){
-  console.log('helloworld')
+  console.log('helloworld!22=dd=')
 }
 export default helloworld
--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist'),
-    clean: true
+    filename: 'bundle.js', // 打包后的文件名
+    path: path.resolve(__dirname, './dist'), // 指定输出目录的根目录
+    clean: true // 每次打包先清除dist内文件
   },
   plugins: [
-    new HtmlWebpackPlugin({
+    new HtmlWebpackPlugin({ // 这个插件可以生成html文件，相关配置文档https://github.com/jantimon/html-webpack-plugin#configuration
       template: './index.html', // 以./index.html为基础打包
       filename: 'app.html', // 新的名字
       inject: 'body', // js放到body中
     })
   ],
-  mode:'development',
+  devServer: {
+    static: './dist',
+  },
+  devtool: 'inline-source-map', // 开始source-map之后。代码出现错误，浏览器能精准定位到那个文件多少行。否则的话。默认指向的是打包后的文件中位置
+  mode:'development', // 打包环境
 }
