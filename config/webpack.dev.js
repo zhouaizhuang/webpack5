35a5f7fccda6134c0109ef3ec5a052e694c06c6e
周爱桩 <840043348@qq.com>
2022-08-17 20:34:41 +0800

    多进程打包

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,6 +1,8 @@
 const path = require('path')
+const os = require('os')
 const ESLintPlugin = require('eslint-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const threads = os.cpus.length
 
 module.exports = {
   entry: './src/main.js',
@@ -39,14 +41,22 @@ module.exports = {
             test: /\.m?js$/,
             exclude: '/node_modules/',
             // include: path.resolve(__dirname, '../src'),
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env'],
-                cacheDirectory: true,
-                cacheCompression: false
+            use: [
+              {
+                loader: 'thread-loader',
+                options: {
+                  works: threads, // 进程数量
+                }
+              },
+              {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env'],
+                  cacheDirectory: true, // 开启babel缓存
+                  cacheCompression: false, // 关闭缓存文件压缩
+                }
               }
-            }
+            ]
           }
         ]
       }
@@ -57,7 +67,8 @@ module.exports = {
       context: path.resolve(__dirname, '../src'),
       exclude: '/node_modules/', // 默认值，不写也是这个效果
       cache: true, // 开启缓存
-      cacheLocation: path.resolve(__dirname, '../node_modules/.cache/eslintcache')
+      cacheLocation: path.resolve(__dirname, '../node_modules/.cache/eslintcache'),
+      threads, // 开启多进程和设置进程数量
     }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../public/index.html')
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,8 +1,11 @@
 const path = require('path')
+const os = require('os')
 const ESLintPlugin = require('eslint-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserWebpackPlugin = require("terser-webpack-plugin")
+const threads = os.cpus.length
 // 封装css Loader获取函数
 function getStyleLoader(pre) {
   return [
@@ -56,14 +59,22 @@ module.exports = {
           {
             test: /\.m?js$/,
             exclude: '/node_modules/',
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env'],
-                cacheDirectory: true, // 开启babel缓存
-                cacheCompression: false, // 关闭缓存文件压缩
+            use: [
+              {
+                loader: 'thread-loader',
+                options: {
+                  works: threads, // 进程数量
+                }
+              },
+              {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env'],
+                  cacheDirectory: true, // 开启babel缓存
+                  cacheCompression: false, // 关闭缓存文件压缩
+                }
               }
-            }
+            ]
           }
         ]
       }
@@ -74,7 +85,8 @@ module.exports = {
       context: path.resolve(__dirname, '../src'),
       exclude: '/node_modules/',
       cache: true, // 开启缓存
-      cacheLocation: path.resolve(__dirname, '../node_modules/.cache/eslintcache')
+      cacheLocation: path.resolve(__dirname, '../node_modules/.cache/eslintcache'),
+      threads, // 开启多进程和设置进程数量
     }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../public/index.html')
@@ -82,8 +94,21 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'static/css/main.css'
     }),
-    new CssMinimizerPlugin()
+    // new CssMinimizerPlugin(),
+    // new TerserWebpackPlugin({
+    //   parallel: threads, // 开启多进程和设置进程数量
+    // })
   ],
+  optimization: {
+    minimizer: [
+      // 压缩css
+      new CssMinimizerPlugin(),
+      // 压缩js(webpack本身是自带js压缩的，但是为了传入多进程配置，就需要手动编写了)
+      new TerserWebpackPlugin({
+        parallel: threads, // 开启多进程和设置进程数量
+      })
+    ]
+  },
   mode: 'production',
   devtool: 'source-map'
 }
