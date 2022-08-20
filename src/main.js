0e80ab32dacfbe979783115d3f8c168d41565884
周爱桩 <840043348@qq.com>
2022-08-20 22:07:57 +0800

    保存其他代码

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import 'core-js'
 import { sum } from './js/count.js'
 import './css/index.css'
 import './css/index.scss'
 import './css/index.less'
 console.log(sum(1, 2))
-
 export const sum1 = [1, 2, 3, 4, 5].reduce((p, c) => p + c, 0)
 
+
+new Promise((resolve)=> {
+  setTimeout(() => {
+    alert('123')
+    resolve('123')
+  }, 1000)
+})
 // if (module.hot) {
 //   // 判断是否支持热模块替换功能
 //   module.hot.accept('./js/count', function (count) {
